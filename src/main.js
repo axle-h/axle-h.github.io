24ac6a31f44e848793c3f77097227e3a375db94d
Alex Haslehurst <alex.haslehurst@gmail.com>
2020-07-24 20:32:59 +0100

    Updated profile

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 "use strict";
 
-require("materialize-css");
+import "materialize-css";
 import HeadlineType from './headline-type';
+import years from './years';
 
 function ready(fn) {
-    if (document.readyState != 'loading'){
+    if (document.readyState !== 'loading'){
       fn();
     } else {
       document.addEventListener('DOMContentLoaded', fn);
     }
   }
-  
+
   ready(function() {
-    var x = 2;
     // Nav.
     new M.Sidenav(document.getElementById("nav-mobile"));
 
@@ -20,5 +20,10 @@ function ready(fn) {
     const headline = document.getElementById("headline-type");
     if (headline) {
       new HeadlineType(headline);
+    }
+
+    const yearsElement = document.getElementById("years");
+    if (yearsElement) {
+      years(yearsElement);
     }
   })
